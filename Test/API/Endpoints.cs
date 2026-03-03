ae3aca8c9dbb2f0a9448c01cd58e068a13403c50
CupOfCakes <heitor.scarv@gmail.com>
2026-03-03 14:07:04 -0300

    feat: create test project and test for GET AllActiveClients api rote

---
+++ Test/API/Endpoints.cs
@@ -0,0 +1,35 @@
+using MeuCafe.Controllers;
+using Domain.Entities;
+using System.Net.Http.Json;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Application.UseCases.Clients.Create;
+
+namespace Test;
+
+public class Endpoints : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public Endpoints(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+
+
+    [Fact]
+    public async Task TestAllActiveClients()
+    {
+        var response = await _client.GetAsync("/api/client/AllActiveClients");
+
+        response.EnsureSuccessStatusCode();
+
+        var clients = await response.Content.ReadFromJsonAsync<List<Client>>();
+
+        Assert.NotNull(clients);
+        Assert.DoesNotContain(clients, c => c.IsActive == false);
+
+    }
+}
